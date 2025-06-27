55066b7a88703005e40b83ce7c7af9e8e58f5187
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2025-06-27 23:00:20 +0530

    zustand store

--- ReactRouter/re-router/src/routes/__root.jsx
+++ ReactRouter/re-router/src/routes/__root.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { useNotification } from "../context/NotificationContext";
+import { useCartStore } from "../store/cartStore";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -10,10 +11,14 @@ export const Route = createRootRoute({
 function RootComponent() {
   // Use the context
   const { count } = useNotification();
+  //Zustand store
+  const cartCount = useCartStore((state) => state.cart.length);
+  // const addToCart = useCartStore((state) => state.addToCart);
   return (
     <React.Fragment>
       <div>Hello "__root"!</div>
       <h3>The value of count is {count}</h3>
+      <h3>The value of cartCount coming from zustand store is {cartCount}</h3>
       <div className="p-2 flex gap-2">
         <Link to="/" className="[&.active]:font-bold">
           Home
